03544a3e38af74b99587db21f963b198e52fbb21
sarangop1728 <santiago.arango@emory.edu>
2022-07-24 00:52:47 -0600

    added single histograms

--- histograms.py
+++ histograms.py
@@ -4,38 +4,56 @@ import csv
 import os
 import imageio
 
-"""
-Copy and paste the output of the moments() function here.
-Coming soon: automatization of this caveman approach.
-"""
 
-''' HISTOGRAM '''
-
+#_____________________________________________________________
 # read data
+#_____________________________________________________________
 
-# a0_data = './stats/d=4/q=2/a0_4_2_10^3.csv'
-def read_sequence_data(file_name):
+
+'''
+read_sequence_data
+
+'''
+
+def read_sequence_data(d,q,n):
     data = []
+    file_name = "./stats/d=" + d + "/q=" + str(q) + '/a0_' + d + '_' + str(q) + '_10^' + str(n) + '.csv'
     with open(file_name, 'r') as F:
         reader = csv.reader(F, quoting=csv.QUOTE_NONNUMERIC)
         for line in reader:
             data.append(line)
     return data
+#_____________________________________________________________
 
-# polys = './stats/d=4/q=2/polys_4_2.txt'
-def read_poly_data(file_name):
+def read_poly_data(d,q):
+    file_name = './stats/d=' +str(d) + '/q=' + str(q) + '/polys_' +str(d) +'_' + str(q) + '.txt'
+    with open(file_name) as F:
+        data = F.read().splitlines()
+    return data
+
+#_____________________________________________________________
+
+def read_moments_data(d,q):
+    file_name = './stats/d=' +str(d) + '/q=' + str(q) + '/moments_' +str(d) +'_' + str(q) + '.txt'
     with open(file_name) as F:
         data = F.read().splitlines()
     return data
         
+#_____________________________________________________________
+# HISTOGRAMS
+#_____________________________________________________________
 
-# buiild all a_0 histograms (10^n data points)  of all q-weil polys of degree d
+'''
+all_histograms: gif for all (d,q)-a0 histograms
+
+*** looks bad when there are too many polynomials.
+
+'''
 def all_histograms(d,q,n):
     buckets = 2**(n-1)*10
-    file_name = "./stats/d=" + str(d) + "/q=" + str(q) + '/a0_' + str(d) + '_' + str(q) + '_10^' + str(n) + '.csv'
     title = 'a0 dist. for ' + str(q) + '-weil polynomials of degree ' + str(d) + ', \n 10^' + str(n) + ' data points and ' + str(buckets) + ' buckets.'
     figure_name = 'a0_' + str(d) + '_' + str(q) + '_10^' + str(n) + '.png'
-    data = read_sequence_data(file_name)
+    data = read_sequence_data(d,q,n)
     r = len(data)
     bins = np.arange(-1,1,2/buckets)
     cols = 7
@@ -51,7 +69,75 @@ def all_histograms(d,q,n):
     plt.savefig(path, dpi=300)
 
 
-# build GIF
+#_____________________________________________________________
+'''
+histogram: histogram for a given (d,q)-Weil polynomial
+
+INPUT: i, index in the list of all (d,q)-Weil polynomials. n, corresponding to 10^n data points.
+
+OUTPUT:
+
+REQUIRES: files 
+'''
+# 'x^4 + 4*x^3 + 8*x^2 + 8*x + 4'
+# './stats/d=4/q=2/a0_4_2_10^2.csv'
+def histogram(polyname,n):
+
+    # get d and q
+    d = polyname[2] # the third letter is always the degree
+    q = str(int(float(polyname[-1])**(2/float(d)))) # the last letter is always the prime power
+    
+    # number of buckets
+    buckets = 2**(n-1)*10
+
+    # get index
+    polydata = read_poly_data(d,q)
+    index = polydata.index(polyname)
+
+    # a0 sequence and moments
+    sequence = read_sequence_data(d,q,n)[index]
+    moments = read_moments_data(d,q)[index]
+    moments = moments.replace("'",'')
+    
+    # remove the '*' from name
+    polyname = polyname.replace('*','')
+
+    title = 'a0 dist. for ' + polyname
+    subtitle = ' 10^' + str(n) + ' data points and ' + str(buckets) + ' buckets.'
+    figure_name = 'a0_' + polyname.replace(' ','') + '_10^' + str(n) + '.png'
+    
+    
+    bins = np.arange(-1,1,2/buckets)
+    fig , ax = plt.subplots()
+    fig.suptitle(title, fontsize=15)
+    ax.set_title(subtitle, fontsize=10)
+    ax.hist(sequence, bins, density = True)
+    ax.set_yticks([])
+    ax.set_xticks([-1,0,1])
+    ax.set_ylim([0,1.6])
+    ax.set_xlim([-1.1,1.1])
+    ax.set_xlabel('moments: ' + moments, fontsize=10)
+    path =  "./stats/d=" + str(d) + "/q=" + str(q) + '/' + figure_name
+    plt.savefig(path, dpi=300)
+
+#_____________________________________________________________
+def d_q_histograms(d,q,n):
+    polydata = read_poly_data(d,q)
+    for polyname in polydata:
+        histogram(polyname,n)
+
+
+
+#_____________________________________________________________
+# GIFs
+#_____________________________________________________________
+
+'''
+gif_all_histograms: gif for all (d,q)-a0 histograms
+
+*** looks bad when there are too many polynomials.
+
+'''
 def gif_all_histograms(d,q):
     
     # build list with images to cicle through
