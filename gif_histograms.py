Fixed crucial bug: the sign of our a1-sequences was wrong

gif_histograms.py
@@ -11,13 +11,9 @@ import sys
 
 
 def read_labels(g,q):
-    
     file_name = './stats/g=' +str(g) + '/q=' + str(q) + '/' +str(g) +'_' + str(q) + '_labels.txt'
-
     with open(file_name) as F:
-
         data = F.read().splitlines()
-
     return data
 
 
@@ -25,27 +21,22 @@ def read_labels(g,q):
 # ______________________________________________________________________________
 
 def gif_histogram(label, exponents = [2,3,4,5], extension = '.png'):
-
-    # Build list with images to cicle through.
+    # Build list with images to cicle through
     images = []
-
-    # Get d and q.
+    # Get d and q
     g = int(label[0])
     q = int(label[2])
-
     file_path =  './stats/g=' + str(g) + '/q=' + str(q) + '/a1_' + label
     for n in exponents:
         for i in range(10): # Repeat same image 10 times.
             file_name = file_path  + '_16^' + str(n) + extension
             images.append(file_name)
-
         if (n == exponents[-1]): # Pause on the last frame.
             file_name = file_path + '_16^' + str(n) + extension
             images.append(file_name)
             for i in range(30): # Repeat same image 30 times.
                 images.append(file_name)
-
-    # Build gif.
+    # Build gif
     gif_name = file_path + '_16^' + str(exponents[-1]) + '.gif'
     # iio.mimsave(gif_name, images, fps=55)
     with iio.get_writer(gif_name, mode='I') as writer:
@@ -53,15 +44,11 @@ def gif_histogram(label, exponents = [2,3,4,5], extension = '.png'):
             image = iio.imread(filename)
             writer.append_data(image)
 
-
 def gif_all_single_histograms(g,q):
-    
     labels = read_labels(g,q)
-
     for label in labels:
         gif_histogram(label)
         print('gif of ' + label + ' has been generated...')
-
 
 # DELETE GARBAGE HISTOGRAMS (n=2,3,4)
 # ______________________________________________________________________________
@@ -82,9 +69,8 @@ def delete_histograms(g,q,exponents=[2,3,4,5],extension='.png'):
 
 # GENERATE DATA
 # ______________________________________________________________________________
-g = 2
-q = 2
+# g = 2
+# q = 2
 # gif_all_single_histograms(g,q)
-
-delete_histograms(g,q)
+# delete_histograms(g,q)
 